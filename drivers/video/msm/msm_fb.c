f15ab4086815761737a315225fd10c78c5532fe9
Oleg Drokin <green@linuxhacker.ru>
2012-01-24 21:03:24 -0500

    Temp hack to avert msm fb crash when you pass in fb= param.
    Otherwise the physical addres without ioremap does not work.
    This is probably better fixed up in the board file once the rest is working.

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -326,7 +326,8 @@ static int msm_fb_probe(struct platform_device *pdev)
 		fbram_size =
 			pdev->resource[0].end - pdev->resource[0].start + 1;
 		fbram_phys = (char *)pdev->resource[0].start;
-		fbram = __va(fbram_phys);
+//		fbram = __va(fbram_phys);
+		fbram = ioremap((unsigned long)fbram_phys, fbram_size);
 
 		if (!fbram) {
 			printk(KERN_ERR "fbram ioremap failed!\n");
