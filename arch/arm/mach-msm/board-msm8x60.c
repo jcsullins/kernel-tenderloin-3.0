46917b9f704b3d8ac567e86de54f3a480b1380ad
James Sullins <jcsullins@gmail.com>
2012-01-25 20:41:17 -0600

    fix compile: wrap WEBCAM_OV9726 code in ifdef

--- arch/arm/mach-msm/board-msm8x60.c
+++ arch/arm/mach-msm/board-msm8x60.c
@@ -10364,11 +10364,13 @@ static void __init msm8x60_init(struct msm_board_data *board_data)
 	else
 		pm8058_platform_data.keypad_pdata = &ffa_keypad_data;
 
+#ifdef CONFIG_WEBCAM_OV9726
 	/* Specify reset pin for OV9726 */
 	if (machine_is_msm8x60_dragon()) {
 		msm_camera_sensor_ov9726_data.sensor_reset = 62;
 		ov9726_sensor_8660_info.mount_angle = 270;
 	}
+#endif
 
 #ifdef CONFIG_BATTERY_MSM8X60
 	if (machine_is_msm8x60_surf() || machine_is_msm8x60_ffa() ||
